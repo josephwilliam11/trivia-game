b9b6e8b052d3762fa40dc6ecf040d4914733ab72
joe arnold <joearnold@joes-MacBook-Air.local>
2017-12-27 14:40:47 -0700

    hw

---
+++ assets/javascript/trivia.js
@@ -0,0 +1,121 @@
+var questions = ['What did Cramer name his pet rooster that he thought was a chicken?' , 'What is the name of the seasonal holiday Georges father invented?' , 'Who catches Jerry and his girlfriend making out during Shildlers List?'];
+
+var triviaQuestions = questions[Math.floor(Math.random() * questions.length)];
+console.log(triviaQuestions);
+
+
+var choices1A = "Newman";
+var choices1B = "Little Jerry";
+var choices1C = "El Pollo Loco";
+var choices1D = "Lois"; 
+
+
+var choices2A = "Bestmas";
+var choices2B = "International Greivances Day"; 
+var choices2C = "Festivus";
+var choices2D = "The Feats of Strength";
+
+
+var choices3A = "Newman";
+var choices3B = "Bania";
+var choices3C = "J. Peterman";
+var choices3D = "David Puddy";
+
+
+
+
+	$("button").click(function() {
+
+var counter = 20;
+var interval = setInterval(function() {
+    counter--;
+    $("#time").html("Time Remaining: " + counter);
+
+    
+    if (counter == 0) {
+        clearInterval(interval);
+    }
+}, 1000);
+for(var i = 0; i < questions.length; i++) {
+$("#questions").html(triviaQuestions);
+
+ }
+
+ if (triviaQuestions === questions[0]) {
+ 	$("#answer1").html(choices1A);
+ 	$("#answer2").html(choices1B);
+ 	$("#answer3").html(choices1C);
+ 	$("#answer4").html(choices1D);
+
+ 	$("#answer1").click(function() {
+ 		$("#questions").html("WRONG!");
+ 		//var incorrect = [src="assets/images/wrong1.gif" , src="assets/images/wrong2.gif" , src="assets/images/wrong3" , src="assets/images/wrong4.gif" , src="assets/images/wrong5.gif"];
+ 		//var wrongAnswer = incorrect[Math.floor(Math.random() * incorrect.length)];
+ 		//$("#choices").html(wrongAnswer);
+ 		//console.log(wrongAnswer);
+ 	});
+
+ 	$("#answer2").click(function() {
+ 		$("#questions").html("CORRECT!");
+ 		//var image = $("<img>").attr("src", imgURL);
+ 		$("#choices").html('<img id="text-center" src="assets/images/littleJerry.gif"/>');
+ 		setTimeout(
+ 			function(){
+ 				var triviaQuestions = questions[Math.floor(Math.random() * questions.length)];
+				$("#questions").html(triviaQuestions);
+ 			},
+ 			3000
+ 			);
+ 	});
+ 	$("#answer3").click(function() {
+ 		$("#questions").html("WRONG!");
+ 		$("#choices").html()
+ 	});
+ 	$("#answer4").click(function() {
+ 		$("#questions").html("WRONG!");
+ 	});
+ }
+
+ if (triviaQuestions === questions[1]) {
+ 	$("#answer1").html(choices2A);
+ 	$("#answer2").html(choices2B);
+ 	$("#answer3").html(choices2C);
+ 	$("#answer4").html(choices2D);
+
+ 	$("#answer1").click(function() {
+ 		$("#questions").html("WRONG!");
+ 	});
+ 	$("#answer2").click(function() {
+ 		$("#questions").html("WRONG!");
+ 	});
+ 	$("#answer3").click(function() {
+ 		$("#questions").html("CORRECT!");
+ 		$("#choices").html('<img id="text-center" src="assets/images/festivus.gif"/>');
+ 	});
+ 	$("#answer4").click(function() {
+ 		$("#questions").html("WRONG!");
+ 	});
+ }
+
+ if (triviaQuestions === questions[2]) {
+ 	$("#answer1").html(choices3A);
+ 	$("#answer2").html(choices3B);
+ 	$("#answer3").html(choices3C);
+ 	$("#answer4").html(choices3D);
+
+ 	$("#answer1").click(function() {
+ 		$("#questions").html("CORRECT!");
+ 		$("#choices").html('<img id="text-center" src="assets/images/newman.gif"/>');
+ 	});
+ 	$("#answer2").click(function() {
+ 		$("#questions").html("WRONG!");
+ 	});
+ 	$("#answer3").click(function() {
+ 		$("#questions").html("WRONG!");
+ 	});
+ 	$("#answer4").click(function() {
+ 		$("#questions").html("WRONG!");
+ 	});
+ }
+
+});
